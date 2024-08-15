35894f752f6fcb35b7896b77a88e9223e3af1527
Amit Shokeen <ashokeen@Amits-MacBook-Pro.local>
2024-08-15 12:38:01 +1000

    append to linked list

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python_data_structures
+1. Append to a linked list
---
+++ linked_list.py
@@ -0,0 +1,22 @@
+class Node:
+    def __init__(self, value) -> None:
+        self.value = value
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value) -> None:
+        new_node = Node(value)
+        self.head = new_node
+        self.length = 1
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.length == 0:
+            self.head = new_node
+        else:
+            current = self.head
+            while current.next is not None:
+                current = current.next
+            current.next = new_node
+        self.length += 1
+        
